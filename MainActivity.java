131d8dbc7bd4243d6bbc80ee720f6560cf1b4fee
htg2704 <49567662+htg2704@users.noreply.github.com>
2019-06-10 12:54:37 +0530

    Gravity Control

--- MainActivity.java
+++ MainActivity.java
@@ -1,50 +1,45 @@
-package com.example.bmicalculator;
+package com.example.gravitycontrol;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText height, weight;
-    TextView result;
-    Button calculate;
+    LinearLayout ll;
+    ImageView imageView;
+    float y;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ll = (LinearLayout) findViewById(R.id.ll);
+        imageView = (ImageView) findViewById(R.id.imv);
 
-        height = (EditText) findViewById(R.id.height);
-        weight = (EditText) findViewById(R.id.weight);
-        result = (TextView) findViewById(R.id.result);
-        calculate = (Button) findViewById(R.id.button);
 
-        calculate.setOnClickListener(new View.OnClickListener() {
+        ll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                calculateBMI();
+                y = imageView.getY();
+                
+                if(y>=0&&y<=100){
+                        ObjectAnimator ay = ObjectAnimator.ofFloat(imageView,"translationY",0f,1800f);
+                ay.setDuration(1000);
+                AnimatorSet as = new AnimatorSet();
+                as.play(ay);
+                as.start();}
+                if(y>100){
+                    ObjectAnimator ay = ObjectAnimator.ofFloat(imageView,"translationY",1800f,0f);
+                    ay.setDuration(1000);
+                    AnimatorSet as = new AnimatorSet();
+                    as.play(ay);
+                    as.start();
+                }
             }
         });
-    }
-
-    private void calculateBMI(){
-        String hstr = height.getText().toString();
-        String wstr = weight.getText().toString();
-
-        if(hstr != null && !"".equals(hstr) && wstr != null && !"".equals(wstr)){
-            float h = Float.parseFloat(hstr)/100;
-            float w = Float.parseFloat(wstr);
-            float bmi = w/(h*h);
-
-            displayBMI(bmi);
-        }
-    }
-
-    private void displayBMI(float bmi){
-        String bmilabel = "" + bmi;
-        result.setText(bmilabel);
     }
 }
